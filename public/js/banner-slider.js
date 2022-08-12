64d8db95e28608c685add11b65a3001ff95712b8
vothiendangkhoa.1999@gmail.com <vothiendangkhoa.1999@gmail.com>
2022-08-12 13:07:23 +0700

    Khoa update splide-product splider banner

---
+++ public/js/banner-slider.js
@@ -0,0 +1,1 @@
+var splide=new Splide(".slide-banner.splide",{type:"loop"});splide.mount();
---
+++ public/js/product-slider.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){new Splide(".splide-product.splide-product",{type:"loop",perPage:5,breakpoints:{1140:{perPage:4},920:{perPage:3},695:{perPage:2},475:{perPage:1}}}).mount()});
